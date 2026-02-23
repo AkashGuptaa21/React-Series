fbf812b646b11887d5cf0405062147c1024a8c4f
Akash Gupta <akashguptaakash9831@gmail.com>
2026-02-23 23:54:44 +0530

    react

---
+++ currencyConvertor/src/Hooks/useCurrencyInfo.js
@@ -0,0 +1,19 @@
+import { useEffect,useState } from "react";
+
+function useCurrencyInfo(currency){
+
+    const [data, setData] =useState({})
+
+    useEffect(()=>{
+
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+         .then((res)=> res.json())   //string to json conversion
+         .then((res) => setData(res[currency]))  //holding converted json into Data variable
+   
+    }, [currency])
+
+    return data 
+}
+
+export default useCurrencyInfo ;
+
